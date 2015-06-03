5bf899c3cec6dec39ceaafd102b875dc1551aa4a
Zachary Demi <zacharydemi@Zacharys-MacBook-Air.local>
2015-06-02 22:28:00 -0400

    added registration form

--- project/sforms.py
+++ project/sforms.py
@@ -1,9 +1,19 @@
-from wtforms import validators, StringField,TextField, IntegerField, SelectField, SelectMultipleField, SubmitField, PasswordField
+from wtforms import validators, StringField,TextField, IntegerField, SelectField, ValidationError, SubmitField, PasswordField
 from flask_wtf import Form
 from wtforms.fields.html5 import EmailField
 
 offices = ['Radnor','Johnstown','Cherry Hill','Pittsburgh','Philadelphia','Princeton']
 positions = ['Senior Consultant','Consultant','Analyst','Sales','Employee Advocate']
+
+#convert new_list into new_tuples
+def tuple_list(new_list):
+    new_tuple = []
+    for x in new_list:
+        insert = (x,x)
+        new_tuple.append(insert)
+
+    new_tuple = sorted(new_tuple)
+    return new_tuple
 
 #custom validator that requires company email
 def custom_email(company):
@@ -11,15 +21,14 @@ def custom_email(company):
 
     def _custom_email(form,field):
         email = field.data
-        if email != "zac_demi@AJG.com":
-        #if email[len(email)-3-len(company):-3] != company:
+        if email[len(email)-4-len(company):-4] != company:
             raise ValidationError(message)
     
     return _custom_email
 
 #email on main page
 class emailOnly(Form):
-    email = EmailField('Company Email',[custom_email('AJG')])
+    email = EmailField('Company Email',validators=[validators.DataRequired(),custom_email('AJG')])
 
 #if user exist then prompt for password
 #email is pulled in from above
@@ -27,12 +36,12 @@ class login(emailOnly):
     password = PasswordField('Enter Password',[validators.DataRequired])
 
 #if user does not exist then redirect to register
-class register(Form):
+class registerUser(Form):
     display_name = StringField('Display name')
-    password = PasswordField('Enter Password',[
+    password = PasswordField('Enter Password',validators=[
         validators.DataRequired,
-        validators.EqualTo('confrim', message = 'Passwords must match')])
+        validators.EqualTo('confirm', message = 'Passwords must match')])
     confirm = PasswordField('Repeat Password')
-    position = SelectField('Job Title',choices=[positions.sort()])
-    office = SelectField('Office',choices=[offices.sort()])
+    position = SelectField('Job Title',choices=tuple_list(positions))
+    office = SelectField('Office',choices=tuple_list(offices))
     
--- project/views.py
+++ project/views.py
@@ -1,13 +1,13 @@
-from flask import request, redirect, render_template, url_for
+from flask import request, redirect, render_template, url_for, flash
 from project import app,mail
 from flask_mail import Message
-from sforms import emailOnly, login, register
+from sforms import emailOnly, login, registerUser
 
 
 @app.route("/",methods = ['GET','POST'])
 def welcome():
     form = emailOnly()
-    if request.method == 'POST' and form.validate:
+    if form.validate_on_submit():
         return "Success!"
     else:
         flash_errors(form)
@@ -17,11 +17,16 @@ def welcome():
 @app.route("/login")
 def login():
     form = login()
+    pass
 
 
-@app.route("/register")
+@app.route("/register", methods = ['GET','POST'])
 def register():
-    form = register()
+    form = registerUser()
+    if form.validate_on_submit():
+        return "Success!"
+
+    return render_template('register.html',form=form)
 
 def flash_errors(form):
     for field, errors in form.errors.items():
