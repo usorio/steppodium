8badc2bad327ff7733335a67f6ef9c9531c82230
Josh Hughes <joshhughes@Flint-Michigan.home>
2015-06-04 23:21:41 -0400

    fixed bugs in login process

--- project/podium.py
+++ project/podium.py
@@ -21,33 +21,33 @@ def sendemail(esubject, esender, erecipients, ehtml):
 
 def sendconfirm(email, user_id):
     erecipients = list(email)
-    esubject, esender, erecipients = 'Registration Confirmed!', 'jmhughes018@gmail.com', ['zac_demi@ajg.com', 'jmhughes018@gmail.com']
+    esubject, esender, erecipients = 'You are almost registered!', 'jmhughes018@gmail.com', ['zac_demi@ajg.com', 'jmhughes018@gmail.com']
     ehtml = render_template('email-premailer.html',user_id=user_id)
     sendemail(esubject, esender, erecipients, ehtml)
 
 def user_exists(email):
-    in_database = users.find({"email":email}).count()
+    in_database = user.find({"email":email}).count()
     if in_database == 0:
         return False
     else:
         return True
 
 def insert_user(email, *args):
-    users.insert({"email":email,"podium_client":"ajg"})
+    user.insert({"email":email,"podium_client":"ajg"})
     user_id = return_id(email)
     sendconfirm(email,user_id)
 
 def update_user(_id,dname,pwd,job,office):
-    users.update({"_id":ObjectId(_id)},{"$set":{"display_name":dname,
+    user.update({"_id":ObjectId(_id)},{"$set":{"display_name":dname,
                 "password":frequency,"job_title":job,"office":office}})
 
 def add_steps(_id,steps):
     date = datetime.now()
     entry = {steps,date}
-    users.update({"_id":ObjectId(_id)},{"$push":{"steps":entry}})
+    user.update({"_id":ObjectId(_id)},{"$push":{"steps":entry}})
 
 def return_id(email):
-    user = users.find_one({"email":email})
+    user = user.find_one({"email":email})
     user_id = user["_id"]
     return user_id
 
